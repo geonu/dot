152eb4b146a83682bf55d622e2f874ace1c5c380
Geonu Lee <ge0nu.two@gmail.com>
2019-05-13 22:01:29 +0900

    Change tmux config
    * https://github.com/tmux/tmux/wiki/FAQ#how-do-i-translate--fg--bg-and--attr-options-into--style-options

--- bin/tmux-theme.sh
+++ bin/tmux-theme.sh
@@ -18,40 +18,34 @@ set -g status-interval 5
 
 
 # default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
+set-option -g status-style fg=$tm_color_active,bg=default
 
 # default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
+set-window-option -g window-status-style fg=$tm_color_inactive,bg=default
 set -g window-status-format "#I #W"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
-set-window-option -g  window-status-current-format "#[bold]#I #W"
+set-window-option -g window-status-current-style fg=$tm_color_active,bg=default
+set-window-option -g window-status-current-format "#[bold]#I #W"
 
 # pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_active_border_color
+set-option -g pane-border-style fg=$tm_color_inactive
+set-option -g pane-active-border-style fg=$tm_active_border_color
 
 # message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+set-option -g message-style bg=default,fg=$tm_color_active
 
-# pane number display
+ # pane number display
 set-option -g display-panes-active-colour $tm_color_active
 set-option -g display-panes-colour $tm_color_inactive
 
-# clock
+ # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
 # battery
 tm_battery="#(~/.dotfiles/bin/battery-indicator.sh)"
 
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
+tm_date="#[fg=$tm_color_inactive] %R %b %d"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]#S"
 
